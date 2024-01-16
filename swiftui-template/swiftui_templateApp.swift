fc5a6847c8bd85c79a97c1bffc657c6b54635c48
MacBookPro <kimani.daniel@eclectics.io>
2024-01-16 09:59:23 +0300

    Initial Commit

---
+++ swiftui-template/swiftui_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_templateApp.swift
+//  swiftui-template
+//
+//  Created by Daniel Kimani on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
